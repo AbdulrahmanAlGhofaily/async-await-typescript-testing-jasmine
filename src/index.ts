f1fe261a0865a4a47abd4590f2a01990daeb7a65
Abdulrahman <abdulrahman-alghofaily@outlook.com>
2022-10-12 21:40:42 +0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,54 @@
+/** Use Axios to get data from restcountries api */
+import axios from "axios";
+
+/** Use the free API https://restcountries.com/
+ * You will use the following endpoints
+ * https://restcountries.com/v2/name/${name} for countries by name
+ * https://restcountries.com/v2/regionalbloc/${regionalbloc} for region blocks
+ */
+
+/** Create getCountry Function here */
+async function getCountry(countryName: string) {
+  const getApi = await axios(
+    `https://restcountries.com/v2/name/${countryName}`
+  );
+  const data = getApi.data[0];
+  const { capital, region, numericCode } = data;
+  return { capital, region, numericCode };
+}
+
+/** Create a test for this getRegion function */
+async function getRegionCountries(regionalbloc: string) {
+  const getApi = await axios(
+    `https://restcountries.com/v2/regionalbloc/${regionalbloc}`
+  );
+  const data = getApi.data;
+  const countries: string[] = [];
+  for (let i = 0; i < data.length; i++) {
+    countries.push(data[i].name);
+  }
+  return countries;
+}
+
+getRegionCountries("nafta");
+
+/** Create getRegionCapitals function here */
+async function getRegionCapitals(regionalbloc: string): Promise<string[]> {
+  const getApi = await axios(
+    `https://restcountries.com/v2/regionalbloc/${regionalbloc}`
+  );
+  const data = getApi.data;
+  const capitals: string[] = [];
+  data.forEach((element: { capital: string }) => {
+    capitals.push(element.capital);
+  });
+  return capitals;
+}
+
+getRegionCapitals("nafta");
+
+export default {
+  getCountry,
+  getRegionCountries,
+  getRegionCapitals,
+};
